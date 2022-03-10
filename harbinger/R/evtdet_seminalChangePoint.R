fbef3f68803240dba96d13c631a2805b3d57a347
Diogo Conde <diogoconde2018@gmail.com>
2022-03-10 13:19:43 -0300

    Update evtdet_seminalChangePoint.R

--- harbinger/R/evtdet_seminalChangePoint.R
+++ harbinger/R/evtdet_seminalChangePoint.R
@@ -1,6 +1,6 @@
 # ==== evtdet_seminalChangePoint: Function for event detection  ====
-# For any given point in time, models are adjusted to segments of data around that 
-# point. Then, the existence of a change point is determined depending on the 
+# For any given point in time, it applies adjustments of models to segments of data around 
+# this given point. After that, the existence of a change point is determined depending on the 
 # quantity of adjustment errors that occur throughout the region. The areas with 
 # most errors are compared with the ones where this rate is the lowest.
 #
